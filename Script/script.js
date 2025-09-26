53dd3f7e6f4bb54e653e9f771be6bad4ef2deeca
Lloyd Simon <lloydsimon12345@gmail.com>
2025-09-26 23:59:41 +0800

    login ok

---
+++ Script/script.js
@@ -0,0 +1,14 @@
+ window.addEventListener('DOMContentLoaded', () => {
+    const loginCard = document.querySelector('.bg-dark.p-4.rounded-4');
+    loginCard.classList.add('login-card-animate');
+  });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.form-control').forEach(input => {
+      input.classList.add('form-control-animate');
+    });
+  });
+
+
+
+
