faf0a75c79e21b57f7d837506177a496c83b20d9
Lloyd Simon <lloydsimon12345@gmail.com>
2025-10-28 23:23:35 +0800

    updating

--- Script/login.js
+++ Script/login.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+ nm // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-analytics.js";
   import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
